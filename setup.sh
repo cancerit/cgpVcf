d69913b2f66afb2d38f9b47dbb10ea4759c728b7
Keiran Raine <keiranmraine@gmail.com>
2014-04-16 16:05:08 +0100

    Added all code required to do one shot install with appropriate patches

---
+++ setup.sh
@@ -0,0 +1,176 @@
+#!/bin/bash
+
+SOURCE_TABIX="http://sourceforge.net/projects/samtools/files/tabix/tabix-0.2.6.tar.bz2/download"
+SOURCE_VCFTOOLS="http://sourceforge.net/projects/vcftools/files/vcftools_0.1.12a.tar.gz/download"
+
+done_message () {
+    if [ $? -eq 0 ]; then
+        echo " done."
+        if [ "x$1" != "x" ]; then
+            echo $1
+        fi
+    else
+        echo " failed.  See setup.log file for error messages." $2
+        echo "    Please check INSTALL file for items that should be installed by a package manager"
+        exit 1
+    fi
+}
+
+get_distro () {
+  EXT=""
+  DECOMP="gunzip -f"
+  if [[ $2 == *.tar.bz2* ]] ; then
+    EXT="tar.bz2"
+    DECOMP="bzip2 -fd"
+  elif [[ $2 == *.tar.gz* ]] ; then
+    EXT="tar.gz"
+  else
+    echo "I don't understand the file type for $1"
+    exit 1
+  fi
+  if hash curl 2>/dev/null; then
+    curl -sS -o $1.$EXT -L $2
+  else
+    wget -nv -O $1.$EXT $2
+  fi
+  mkdir -p $1
+  `$DECOMP $1.$EXT`
+  tar --strip-components 1 -C $1 -xf $1.tar
+}
+
+if [ "$#" -ne "1" ] ; then
+  echo "Please provide an installation path  such as /opt/pancan"
+  exit 0
+fi
+
+INST_PATH=$1
+
+CPU=`grep -c ^processor /proc/cpuinfo`
+if [ $? -eq 0 ]; then
+  if [ "$CPU" -gt "6" ]; then
+    CPU=6
+  fi
+else
+  CPU=1
+fi
+echo "Max compilation CPUs set to $CPU"
+
+# get current directory
+INIT_DIR=`pwd`
+
+# re-initialise log file
+echo > $INIT_DIR/setup.log
+
+# log information about this system
+(
+    echo '============== System information ===='
+    set -x
+    lsb_release -a
+    uname -a
+    sw_vers
+    system_profiler
+    grep MemTotal /proc/meminfo
+    set +x
+    echo; echo
+) >>$INIT_DIR/setup.log 2>&1
+
+# cleanup inst_path
+mkdir -p $INST_PATH/bin
+cd $INST_PATH
+INST_PATH=`pwd`
+cd $INIT_DIR
+
+# make sure that build is self contained
+unset PERL5LIB
+ARCHNAME=`perl -e 'use Config; print $Config{archname};'`
+PERLROOT=$INST_PATH/lib/perl5
+PERLARCH=$PERLROOT/$ARCHNAME
+export PERL5LIB="$PERLROOT:$PERLARCH"
+
+#create a location to build dependencies
+SETUP_DIR=$INIT_DIR/install_tmp
+mkdir -p $SETUP_DIR
+
+cd $SETUP_DIR
+
+CURR_TOOL="tabix"
+CURR_SOURCE=$SOURCE_TABIX
+echo -n "Building $CURR_TOOL ..."
+if [ -e $SETUP_DIR/$CURR_TOOL.success ]; then
+  echo -n " previously installed ..."
+else
+  (
+    set -ex
+    get_distro $CURR_TOOL $CURR_SOURCE
+    cd $SETUP_DIR/$CURR_TOOL
+    make -j$CPU
+    cd perl
+    patch Makefile.PL < $INIT_DIR/patches/tabixPerlLinker.diff
+    perl Makefile.PL INSTALL_BASE=$INST_PATH
+    make
+    make test
+    make install
+    touch $SETUP_DIR/$CURR_TOOL.success
+  ) >>$INIT_DIR/setup.log 2>&1
+fi
+done_message "" "Failed to build $CURR_TOOL."
+
+cd $SETUP_DIR
+
+CURR_TOOL="vcftools"
+CURR_SOURCE=$SOURCE_VCFTOOLS
+echo -n "Building $CURR_TOOL ..."
+if [ -e $SETUP_DIR/$CURR_TOOL.success ]; then
+  echo -n " previously installed ..."
+else
+  (
+    set -ex
+    get_distro $CURR_TOOL $CURR_SOURCE
+    cd $SETUP_DIR/$CURR_TOOL
+    patch Makefile < $INIT_DIR/patches/vcfToolsInstLocs.diff
+    patch perl/Vcf.pm < $INIT_DIR/patches/vcfToolsProcessLog.diff
+    make -j$CPU PREFIX=$INST_PATH
+    touch $SETUP_DIR/$CURR_TOOL.success
+  ) >>$INIT_DIR/setup.log 2>&1
+fi
+done_message "" "Failed to build $CURR_TOOL."
+
+#add bin path for install tests
+export PATH="$INST_PATH/bin:$PATH"
+
+cd $INIT_DIR
+
+echo -n "Installing Perl prerequisites ..."
+if ! ( perl -MExtUtils::MakeMaker -e 1 >/dev/null 2>&1); then
+    echo
+    echo "WARNING: Your Perl installation does not seem to include a complete set of core modules.  Attempting to cope with this, but if installation fails please make sure that at least ExtUtils::MakeMaker is installed.  For most users, the best way to do this is to use your system's package manager: apt, yum, fink, homebrew, or similar."
+fi
+(
+  set -x
+  $INIT_DIR/bin/cpanm -v --mirror http://cpan.metacpan.org -notest -l $INST_PATH/ --installdeps . < /dev/null
+  set +x
+) >>$INIT_DIR/setup.log 2>&1
+done_message "" "Failed during installation of core dependencies."
+
+echo -n "Installing cgpVcf ..."
+(
+  cd $INIT_DIR
+  perl Makefile.PL INSTALL_BASE=$INST_PATH
+  make
+  make test
+  make install
+) >>$INIT_DIR/setup.log 2>&1
+done_message "" "cgpVcf install failed."
+
+# cleanup all junk
+rm -rf $SETUP_DIR
+
+echo
+echo
+echo "Please add the following to beginning of path:"
+echo "  $INST_PATH/bin"
+echo "Please add the following to beginning of PERL5LIB:"
+echo "  $PERLROOT"
+echo
+
+exit 0
