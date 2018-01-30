da955078b771da7e913082436b884021918977d3
Keiran Raine <kr2@sanger.ac.uk>
2018-01-30 12:02:09 +0000

    vcfTools 0.1.15 incorporates our patch

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ########## LICENCE ##########
-# Copyright (c) 2014-2016 Genome Research Ltd.
+# Copyright (c) 2014-2018 Genome Research Ltd.
 #
 # Author: Cancer Genome Project <cgpit@sanger.ac.uk>
 #
@@ -31,20 +31,7 @@
 # 2009, 2010, 2011, 2012’."
 ########## LICENCE ##########
 
-SOURCE_VCFTOOLS="https://github.com/vcftools/vcftools/releases/download/v0.1.14/vcftools-0.1.14.tar.gz"
-
-done_message () {
-    if [ $? -eq 0 ]; then
-        echo " done."
-        if [ "x$1" != "x" ]; then
-            echo $1
-        fi
-    else
-        echo " failed.  See setup.log file for error messages." $2
-        echo "    Please check INSTALL file for items that should be installed by a package manager"
-        exit 1
-    fi
-}
+SOURCE_VCFTOOLS="https://github.com/vcftools/vcftools/releases/download/v0.1.15/vcftools-0.1.15.tar.gz"
 
 get_distro () {
   EXT=""
@@ -105,22 +92,6 @@ echo "Max compilation CPUs set to $CPU"
 # get current directory
 INIT_DIR=`pwd`
 
-# re-initialise log file
-echo > $INIT_DIR/setup.log
-
-# log information about this system
-(
-    echo '============== System information ===='
-    set -x
-    lsb_release -a
-    uname -a
-    sw_vers
-    system_profiler
-    grep MemTotal /proc/meminfo
-    set +x
-    echo; echo
-) >>$INIT_DIR/setup.log 2>&1
-
 set -e
 
 # cleanup inst_path
@@ -163,14 +134,12 @@ if [ -e $SETUP_DIR/$CURR_TOOL.success ]; then
   echo -n " previously installed ..."
 else
   get_distro $CURR_TOOL $CURR_SOURCE
-  cd $SETUP_DIR/$CURR_TOOL && \
-  patch src/perl/Vcf.pm < $INIT_DIR/patches/vcfToolsProcessLog.diff && \
-  ./configure --prefix=$INST_PATH --with-pmdir=$INST_PATH/lib/perl5 && \
-  make -j$CPU && \
-  make install && \
+  cd $SETUP_DIR/$CURR_TOOL
+  ./configure --prefix=$INST_PATH --with-pmdir=$INST_PATH/lib/perl5
+  make -j$CPU
+  make install
   touch $SETUP_DIR/$CURR_TOOL.success
 fi
-done_message "" "Failed to build $CURR_TOOL."
 
 cd $INIT_DIR
 
@@ -180,7 +149,6 @@ if ! ( perl -MExtUtils::MakeMaker -e 1 >/dev/null 2>&1); then
     echo "WARNING: Your Perl installation does not seem to include a complete set of core modules.  Attempting to cope with this, but if installation fails please make sure that at least ExtUtils::MakeMaker is installed.  For most users, the best way to do this is to use your system's package manager: apt, yum, fink, homebrew, or similar."
 fi
 $CPANM --mirror http://cpan.metacpan.org --notest -l $INST_PATH/ --installdeps . < /dev/null
-done_message "" "Failed during installation of core dependencies."
 
 echo -n "Installing cgpVcf ..."
 cd $INIT_DIR
